db1fac2120cb70386c47320f32c7519a74802363
Guslington <guslington@gmail.com>
2020-06-25 13:33:02 +1000

    set az count on es cluster to set multiple subnets

--- elasticsearch.cfhighlander.rb
+++ elasticsearch.cfhighlander.rb
@@ -3,6 +3,7 @@ CfhighlanderTemplate do
   Parameters do
     ComponentParam 'EnvironmentName', 'dev', isGlobal: true
     ComponentParam 'EnvironmentType', 'development', allowedValues: ['development','production'], isGlobal: true
+    ComponentParam 'AvailabilityZones', 1, isGlobal: true, allowedValues: [1,2,3]
     
     ComponentParam 'VPCId', type: 'AWS::EC2::VPC::Id'
     ComponentParam 'Subnets'
